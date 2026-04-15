4c10a65e315ee5ef59f2ca5c03bfa0a2ef579e7f
Kamron <odilovkamron14@gmail.com>
2026-04-15 19:48:00 +0500

    Javascript1

---
+++ app.js
@@ -0,0 +1,141 @@
+let baho = 3
+if (baho >= 5) {
+    console.log("Zo'r")
+}
+else if (baho >= 4) {
+    console.log("Yaxshi")
+}
+else if (baho >= 3) {
+    console.log("Harakat qil")
+}
+else {
+    console.log("Yana o'qi")
+}
+
+let yosh = 17
+if (yosh >= 18) {
+    console.log("Senga multfilm ko'rish mumkin")
+}
+else if (yosh <= 18) {
+    console.log("Senga jangovar ko'rish mumkin emas ")
+}
+
+let harorat = 14
+if (harorat <= 15) {
+    console.log("Issiq kiyin")
+}
+else {
+    console.log("Yengil kiyinishing mumkin")
+}
+
+let parol = "12345"
+if (parol === "12345") {
+    console.log("Xush kelibsiz")
+}
+else {
+    console.log("Eshik yopiq")
+}
+
+let bolish = 4
+if (bolish % 2 === 0) {
+    console.log("Juft")
+}
+else {
+    console.log("Toq")
+}
+
+let hafta = 3
+switch (hafta) {
+    case 1:
+        console.log("dushanba")
+        break;
+    case 2:
+        console.log("Seshanba")
+        break;
+    case 3:
+        console.log("Chorshanba")
+        break;
+    case 4:
+        console.log("Payshanba")
+        break;
+    case 5:
+        console.log("Juma")
+        break;
+    case 6:
+        console.log("Shanba")
+        break;
+    case 7:
+        console.log("Yakshanba")
+        break;
+    default:
+        console.log("Noto'g'ri hafta kiritildi")
+}
+
+let svetofor = 2
+switch (svetofor) {
+    case 1:
+        console.log("To'xta")
+        break;
+    case 2:
+        console.log("Tayyorlan")
+        break;
+    case 3:
+        console.log("Yur")
+        break;
+}
+
+let fasl = 12
+switch (fasl) {
+    case 1:
+        console.log("Qish")
+        break;
+    case 2:
+        console.log("Qish")
+        break;
+    case 3:
+        console.log("Bahor")
+        break;
+    case 4:
+        console.log("Bahor")
+        break;
+    case 5:
+        console.log("Bahor")
+        break;
+    case 6:
+        console.log("Yoz")
+        break;
+    case 7:
+        console.log("Yoz")
+        break;
+    case 8:
+        console.log("Yoz")
+        break;
+    case 9:
+        console.log("Kuz")
+        break;
+    case 10:
+        console.log("Kuz")
+        break;
+    case 11:
+        console.log("Kuz")
+        break;
+    case 12:
+        console.log("Qish")
+        break;
+}
+
+let narx = 1
+switch(narx) {
+    case 1:
+        console.log("Olma: 5000 so'm")
+        break;
+         case 2:
+        console.log("Banan: 9000 so'm")
+        break;
+         case 3:
+        console.log("Nok: 8000 so'm")
+        break;
+        default:
+            console.log("Bizda bunaqa meva yo'q")
+}
+
